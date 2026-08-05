0dba43f33794156012168eaf800e67b1cdb7a2a3
mamugo1432 <mamugo1432@gmail.com>
2026-08-05 11:54:46 +0200

    Adding pom

---
+++ src/test/java/es/ia/translator/TranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ia.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
